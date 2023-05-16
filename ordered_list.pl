61f510be59024e197d76f853004a8cfac19936f0
Saqib Javed <jvedsaqib6291@gmail.com>
2023-05-16 20:19:08 +0530

    changes

--- ordered_list.pl
+++ ordered_list.pl
@@ -1,7 +1,19 @@
-ordered([_]) :-
-    !.
+checkList(L):-
+    write('Given List'),
+    write(L),
+    write('\n'),
+    xsort(L,L1),
+    write(L1),!.
 
-ordered([X,Y|Z]) :-
-    X =< Y,
-    ordered([Y|Z]).
+xsort([X,Y],[Y,X]):-
+    X>Y,!.
+xsort([X,Y],[X,Y]):-
+    X=<Y,!.
 
+xsort([X,Y|Z],[Y|L1]):-
+    X>Y,
+    xsort([X|Z],L1).
+
+xsort([X,Y|Z],[X|L1]):-
+    X=<Y,
+    xsort([Y|Z],L1).
